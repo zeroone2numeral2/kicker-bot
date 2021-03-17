893f9a25e8711fa941e873a1edb5334e189e78bb
zeroone2numeral2 <numeralzeroone@gmail.com>
2021-03-17 17:31:31 +0100

    Make sure we only receive "message" updates

--- main.py
+++ main.py
@@ -243,8 +243,10 @@ def main():
 
     updater.bot.set_my_commands([])  # make sure the bot doesn't have any command set
 
-    logger.info("running as @%s", updater.bot.username)
-    updater.start_polling(drop_pending_updates=True)
+    allowed_updates = ["message"]  # https://core.telegram.org/bots/api#getupdates
+
+    logger.info("running as @%s, allowed updates: %s", updater.bot.username, allowed_updates)
+    updater.start_polling(drop_pending_updates=True, allowed_updates=allowed_updates)
 
 
 if __name__ == '__main__':
